0863a355821b0844c5c0f9800c50b4c7288cf500
alexwololo <alexander.caspersson@gmail.com>
2022-10-27 13:40:08 +0200

    logrocket3

--- azure/src/App.js
+++ azure/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LogRocket from 'logrocket';
-
+LogRocket.init('swrfab/azure');
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        LogRocket.init('swrfab/azure');
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
